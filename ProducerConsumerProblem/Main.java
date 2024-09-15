790d6d09dc9873a30febafd0f1d15d8921628ef0
abhinav rastogi <56015300+abhinav2188@users.noreply.github.com>
2024-09-15 12:36:54 +0000

    pc problem initial

---
+++ ProducerConsumerProblem/Main.java
@@ -0,0 +1,29 @@
+package ProducerConsumerProblem;
+
+public class Main {
+    
+    // problem -- we have a space with n capacity
+    // at a time either item can be produced or it can be consumed
+    // a producer can produce an item only when there is empty space
+    // a consumer can consume item if one of the space is filled
+
+    public static void main(String[] args) {
+        
+        SharedResource sharedResource = new SharedResource(5);
+
+        Thread producer = new Thread(()->{
+            sharedResource.produceItem();
+        });
+
+        Thread consumer = new Thread(()->{
+            sharedResource.consumeItem();
+        });
+    
+
+
+    
+    }
+
+
+
+}
---
+++ ProducerConsumerProblem/SharedResource.java
@@ -0,0 +1,42 @@
+package ProducerConsumerProblem;
+
+public class SharedResource {
+    int instancesCount;
+    int emptyCount;
+    // int fullCount;
+
+    public SharedResource(int instancesCount){
+        this.instancesCount = instancesCount;
+        this.emptyCount = instancesCount;
+        // this.fullCount = 0; 
+
+    }
+
+    public synchronized void produceItem(){
+        if(emptyCount>0){
+            try{
+                Thread.sleep(3000);
+            }catch(Exception e){
+                // handle exception
+            }
+    
+            System.out.println("item is produced");
+            emptyCount--;
+        }else{
+            System.out.println("no empty space");
+        }
+    }
+
+    public synchronized void consumeItem(){
+        while(emptyCount == instancesCount
+)
+        if(emptyCount<instancesCount){
+            emptyCount++;
+            System.out.println("item is consumed");
+        }else{
+            System.out.println("all items consumed");
+        }
+    }
+
+
+}
